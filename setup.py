99ffd0c9ff85dfd7bf9d64083045ea4d68a3bc51
Hriddhi Dey <hriddhidey@gmail.com>
2017-09-04 12:55:37 +0530

    Included setuptools fallback.

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 """setup.py for visage."""
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='visage',
     packages=['visage'],
-    version='1',
+    version='0.1',
     description='Add virtual makeup to an image of a face.',
     author='Hriddhi Dey',
     author_email='hriddhidey@gmail.com',
     url='https://github.com/hriddhidey/visage.git',
-    download_url='https://github.com/hriddhidey/visage/archive/1.tar.gz',
+    download_url='https://github.com/hriddhidey/visage/archive/0.1.tar.gz',
     install_requires=[
         'opencv-python',
         'scikit-image',
